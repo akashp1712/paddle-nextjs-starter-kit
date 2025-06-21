617c81f4fd943c457da8d7a40e3a99af04e37970
Akash Panchal <akashp1712@gmail.com>
2025-06-21 22:35:14 +0530

    Updated route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import { getPaddleInstance } from '@/utils/paddle/get-paddle-instance';
 const webhookProcessor = new ProcessWebhook();
 
 export async function POST(request: NextRequest) {
-  const signature = request.headers.get('paddle-signature') || '';
+  const signature = awit request.headers.get('paddle-signature') || '';
   const rawRequestBody = await request.text();
   const privateKey = process.env['PADDLE_NOTIFICATION_WEBHOOK_SECRET'] || '';
 
